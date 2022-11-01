3c39b1fdf1f3029e0061a60a1391b7593c704f1f
Piotr Mroczkowski <p.mrocz@vp.pl>
2022-11-01 15:11:15 +0100

    Add helper for enum

---
+++ WarehouseManagerApi.Application/Helpers/EnumHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagerApi.Domain.Exceptions;
+
+namespace WarehouseManagerApi.Application.Helpers;
+public static class EnumHelper
+{
+    public static string GetEnumDescription(this Enum enumValue)
+    {
+        var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+        if (fieldInfo == null)
+            throw new ApiException("Enum value not found");
+
+        var descriptionAttributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        return descriptionAttributes.Length > 0 ? descriptionAttributes[0].Description : enumValue.ToString();
+    }
+}
