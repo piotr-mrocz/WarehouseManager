c26ac5bf800a3a0a0b71714a85f9abbd5bd0150a
Piotr Mroczkowski <p.mrocz@vp.pl>
2022-11-05 21:41:35 +0100

    Add first validator

---
+++ WarehouseManagerApi.Application/Features/Product/Validators/AcceptTheGoodCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagerApi.Domain.Dtos;
+using WarehouseManagerApi.Domain.Entities;
+
+namespace WarehouseManagerApi.Application.Features.Product.Validators;
+public class AcceptTheGoodCommandValidator : AbstractValidator<AcceptTheGoodCommand>
+{
+	public AcceptTheGoodCommandValidator()
+	{
+		RuleFor(x => x.AcceptTheGoodDto.IdProduct)
+            .NotNull()
+			.NotEmpty()
+			.GreaterThan(default(int))
+			.WithMessage("Nie podano produktu!");
+
+		RuleFor(x => x.AcceptTheGoodDto.Quantity)
+            .NotNull()
+            .NotEmpty()
+            .GreaterThan(default(uint))
+            .WithMessage("Nie podano ilości lub jej wartość jest równa 0!");
+
+		RuleFor(x => x.AcceptTheGoodDto.Weight)
+            .NotNull()
+            .NotEmpty()
+            .GreaterThan(default(decimal))
+            .WithMessage("Nie podano wagi lub jej wartość jest równa 0!");
+    }
+}
---
+++ WarehouseManagerApi.Domain/Dtos/AcceptTheGoodDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagerApi.Domain.Dtos;
+public class AcceptTheGoodDto
+{
+    public int IdProduct { get; set; }
+    public string? ExtraInfo { get; set; } = null;
+    public uint Quantity { get; set; }
+    public decimal Weight { get; set; }
+}
